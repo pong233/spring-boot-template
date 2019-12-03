b6e0b5b7a9afc19fd5e70b5599ec6beff3984c1f
shumingd <1284786859@qq.com>
2019-12-03 22:34:31 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    @RequestMapping("/")
+    public String helloWorld() {
+
+        return "helloWorld";
+    }
+
+
+}
